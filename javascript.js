02831db4591979bc8ba072fc3aab5090dcab4d3b
Matthew Burgess <boog397@gmail.com>
2024-02-17 15:31:48 -0600

    Add operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        default:
+            return divide(num1, num2);
+            break;
+    }
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
